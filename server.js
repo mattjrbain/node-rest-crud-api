1d471dcf1b315115fe3069902caaf90dc0bf2431
matt.zaph <zmcbain@icloud.com>
2020-02-20 17:51:40 +0100

    Bit of security

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 let cors = require('cors');
+let corsOptions = {
+    origin: 'http://localhost:3000'
+};
+
 let app = express();
 let bodyParser = require('body-parser');
 let mysql = require('mysql');
@@ -7,6 +11,7 @@ const cnx = require('./config/connexionBdd.js');
 const categoriesService = require('./services/categories');
 const produitsService = require('./services/produits');
 const {check, validationResult} = require('express-validator');
+const helmet = require('helmet');
 
 app.use(cors());
 app.use(express.json());
@@ -14,22 +19,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(helmet());
 
 
 // Default route
 
-app.get('/', function (req, res) {
+app.get('/', cors(corsOptions), function (req, res) {
     return res.send({error: true, message: 'hello'})
 });
 
 // Retrieve all categories
-app.get('/categories', categoriesService.getAll);
+app.get('/categories', cors(corsOptions), categoriesService.getAll);
 
 // Retrieve category with id
-app.get('/categories/:id', categoriesService.getOneById);
+app.get('/categories/:id', cors(corsOptions), categoriesService.getOneById);
 
 // Add a new category
-app.post('/categories',
+//TODO: flash message
+app.post('/categories', cors(corsOptions),
     check('libelle', 'Le libelle ne doit contenir que des lettres').isAlpha(),
     check('libelle', 'Mauvaise longueur').isLength({min: 3, max: 10}),
     categoriesService.createCat
@@ -37,15 +44,15 @@ app.post('/categories',
 ;
 
 //  Update category with id
-app.put('/categories/:id', categoriesService.updateCat);
+app.put('/categories/:id', cors(corsOptions), categoriesService.updateCat);
 
 //  Delete category
-app.delete('/categories/:id', categoriesService.delete);
+app.delete('/categories/:id', cors(corsOptions), categoriesService.delete);
 
 
 // Produits
 // Retrieve produits by category id
-app.get('/categories/:id/produits', produitsService.getProduitsByCatId);
+app.get('/categories/:id/produits', cors(corsOptions), produitsService.getProduitsByCatId);
 
 
 // set port
